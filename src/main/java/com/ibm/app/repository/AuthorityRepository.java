4e1aae4cbe81cabb6cb8b629cafa473fca8840e0
VINIT BHATNAGAR <vinit.bhatnagar@in.ibm.com>
2019-09-21 23:24:01 +0530

    Initial application generated by JHipster-6.2.0

---
+++ src/main/java/com/ibm/app/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.ibm.app.repository;
+
+import com.ibm.app.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/ibm/app/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.ibm.app.web.rest.vm;
